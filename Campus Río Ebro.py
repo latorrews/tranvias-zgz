622cc9f2a7a47f1b8aa60082cabd7ad11dbb112b
Javier Latorre <65726176+latorrews@users.noreply.github.com>
2022-05-24 20:15:05 +0200

    Añadir los scripts para cada parada

---
+++ Campus Río Ebro.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+r1 = requests.get(url = "https://www.zaragoza.es/api/recurso/urbanismo-infraestructuras/tranvia/401.json?srsname=wgs84")
+data1 = r1.json()
+
+r2 = requests.get(url = "https://www.zaragoza.es/api/recurso/urbanismo-infraestructuras/tranvia/402.json?srsname=wgs84")
+data2 = r2.json()
+
+destino11 = data1["destinos"][0]["destino"]
+tiempo11 = data1["destinos"][0]["minutos"]
+destino12 = data1["destinos"][1]["destino"]
+tiempo12 = data1["destinos"][1]["minutos"]
+
+destino21 = data2["destinos"][0]["destino"]
+tiempo21 = data2["destinos"][0]["minutos"]
+destino22 = data2["destinos"][1]["destino"]
+tiempo22 = data2["destinos"][1]["minutos"]
+
+print("El primer tranvía con destino a ", destino11, " tarda ", tiempo11, " minutos.")
+print("El segundo tranvía con destino a ", destino12, " tarda ", tiempo12, " minutos.")
+print("El tercer tranvía con destino a ", destino21, " tarda ", tiempo21, " minutos.")
+print("El cuarto tranvía con destino a ", destino22, " tarda ", tiempo22, " minutos.")
---
+++ Margarita Xirgu.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+r = requests.get(url = "https://www.zaragoza.es/api/recurso/urbanismo-infraestructuras/tranvia/501.json?srsname=wgs84")
+data = r.json()
+
+destino1 = data["destinos"][0]["destino"]
+tiempo1 = data["destinos"][0]["minutos"]
+destino2 = data["destinos"][1]["destino"]
+tiempo2 = data["destinos"][1]["minutos"]
+
+print("El primer tranvía con destino a ", destino1, " tarda ", tiempo1, " minutos.")
+print("El segundo tranvía con destino a ", destino2, " tarda ", tiempo2, " minutos.")
