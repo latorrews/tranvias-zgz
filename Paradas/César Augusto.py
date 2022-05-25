6b1b8cce3d4f46152bae7c32de11997d57a1c871
Javier Latorre <65726176+latorrews@users.noreply.github.com>
2022-05-25 07:38:48 +0000

    Mejorar usabilidad

--- Paradas/César Augusto.py
+++ Paradas/César Augusto.py
@@ -17,7 +17,23 @@ tiempo21 = data2["destinos"][0]["minutos"]
 destino22 = data2["destinos"][1]["destino"]
 tiempo22 = data2["destinos"][1]["minutos"]
 
-print("El primer tranvía con destino a ", destino11, " tarda ", tiempo11, " minutos.")
-print("El segundo tranvía con destino a ", destino12, " tarda ", tiempo12, " minutos.")
-print("El tercer tranvía con destino a ", destino21, " tarda ", tiempo21, " minutos.")
-print("El cuarto tranvía con destino a ", destino22, " tarda ", tiempo22, " minutos.")
+if tiempo11 == 0:
+  print("El primer tranvía con destino a ", destino11, " ya está en la parada.")
+else:
+  print("El primer tranvía con destino a ", destino11, " tarda ", tiempo11, " minutos.")
+
+if tiempo12 == 0:
+  print("El segundo tranvía con destino a ", destino12, " ya está en la parada.")
+else:
+  print("El segundo tranvía con destino a ", destino12, " tarda ", tiempo12, " minutos.")
+
+
+if tiempo21 == 0:
+  print("El tercer tranvía con destino a ", destino21, " ya está en la parada.")
+else:
+  print("El tercer tranvía con destino a ", destino21, " tarda ", tiempo21, " minutos.")
+
+if tiempo22 == 0:
+  print("El cuarto tranvía con destino a ", destino22, "ya está en la parada.")
+else:
+  print("El cuarto tranvía con destino a ", destino22, " tarda ", tiempo22, " minutos.")
--- Paradas/Los Pájaros.py
+++ Paradas/Los Pájaros.py
@@ -9,5 +9,12 @@ tiempo1 = data["destinos"][0]["minutos"]
 destino2 = data["destinos"][1]["destino"]
 tiempo2 = data["destinos"][1]["minutos"]
 
-print("El primer tranvía con destino a ", destino1, " tarda ", tiempo1, " minutos.")
-print("El segundo tranvía con destino a ", destino2, " tarda ", tiempo2, " minutos.")
+if tiempo1 == 0:
+  print("El primer tranvía con destino a ", destino1, " ya está en la parada.")
+else:
+  print("El primer tranvía con destino a ", destino1, " tarda ", tiempo1, " minutos.")
+
+if tiempo2 == 0:
+  print("El segundo tranvía con destino a ", destino2, " ya está en la parada.")
+else:
+  print("El segundo tranvía con destino a ", destino2, " tarda ", tiempo2, " minutos.")
